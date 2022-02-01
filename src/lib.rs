7996afb1a0d0c39e0f948a400bdbac6400756b66
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-02-01 07:19:39 +0700

    feat: `/subscriptions` endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,28 @@ use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health-check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscriptions))
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
+
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health-check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+#[derive(serde::Deserialize)]
+struct SubscriptionData {
+    name: String,
+    email: String,
+}
 
-    Ok(server)
+async fn subscriptions(_data: web::Form<SubscriptionData>) -> impl Responder {
+    HttpResponse::Ok()
 }
--- tests/health-check.rs
+++ tests/health-check.rs
@@ -2,17 +2,63 @@ use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
-    let url = spawn_app();
+    let app_address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get(format!("{}/health-check", url))
+        .get(format!("{}/health-check", app_address))
         .send()
         .await
         .expect("Failed to execute request.");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_200_if_data_valid() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Simulate data from a url-encoded form
+    let body = "name=danil%hendra&email=danilhendrasr%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_if_data_invalid() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=danil%20hendra", "missing email"),
+        ("email=danilhendrasr%40gmail.com", "missing name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    }
 }
 
 fn spawn_app() -> String {

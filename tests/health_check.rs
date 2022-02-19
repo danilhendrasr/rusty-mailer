4d2d3146234245c041a81d120448d6bdac387608
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-02-19 21:34:16 +0700

    refactor: make logging available to use in test suites

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,9 @@
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DBSettings};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::test]
 async fn health_check_works() {
@@ -74,12 +76,27 @@ async fn subscribe_returns_400_if_data_invalid() {
     }
 }
 
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
+
 pub struct TestApp {
     address: String,
     db_pool: PgPool,
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let our_port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", our_port);

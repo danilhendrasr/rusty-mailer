0aa3aec63760d96e8c0ca74fe607c76585dec256
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-03-17 04:26:28 +0700

    feat(schema): add status to subscriptions entity

---
+++ migrations/20220316212340_make_status_not_null_in_subscriptions.sql
@@ -0,0 +1,8 @@
+-- Add migration script here
+BEGIN;
+  UPDATE subscriptions
+    SET status = 'confirmed'
+    WHERE status IS NULL;
+  
+  ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
+COMMIT;
